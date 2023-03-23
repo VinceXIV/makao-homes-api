b235a641d56982a3a1ae771a9d09859f8839bb4e
Vincent <makokhavomondi@gmail.com>
2023-03-23 11:52:58 +0300

    Get bcrypt to handle the password thingy

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,6 +1,8 @@
 class Seller < ApplicationRecord
     has_many :houses
 
+    has_secure_password
+
     validates :username, presence: true
     validates :email, presence: true
     validates :password, presence: true
